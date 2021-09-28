64a4cce985a739b5a47512a276ad2bb7fe7a2c1e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-09-28 18:15:21 -0500

    Add content to setup.py

--- setup.py
+++ setup.py
@@ -1,60 +1,44 @@
 from setuptools import setup
 
-from ssl_metrics_MODULE_NAME import version
+from pp_me import version
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="ssl-metrics-MODULE-NAME",
-    packages=["ssl_metrics_MODULE_NAME"],
+    name="pp-me",
+    packages=["pp_me"],
     version=version.version(),
-    description="SSL Metrics - SHORT DESCRIPTION",
-    author="Software and Systems Laboratory - Loyola University Chicago",
-    author_email="ssl-metrics@ssl.luc.edu",
+    description="Pretty Print Me!",
+    author="Nicholas M. Synovic",
+    author_email="nicholas.synovic@gmail.com",
     license="BSD",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://ssl.cs.luc.edu/projects/metricsDashboard",
+    url="https://nicholassynovic.github.io",
     project_urls={
-        "Bug Tracker": "https://github.com/SoftwareSystemsLaboratory/ssl-metrics-REPOSITORY-NAME/issues",
-        "GitHub Repository": "https://github.com/SoftwareSystemsLaboratory/ssl-metrics-REPOSITORY-NAME",
+        "Bug Tracker": "https://github.com/NicholasSynovic/pp-me/issues",
+        "GitHub Repository": "https://github.com/NicholasSynovic/pp-me",
     },
-    keywords=[
-        "git",
-        "github",
-        "software engineering",
-        "metrics",
-        "software systems laboratory",
-        "ssl",
-        "loyola",
-        "loyola university chicago",
-        "luc",
-    ],
+    keywords=["pretty", "terminal", "json"],
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Software Development",
-        "Topic :: Scientific/Engineering",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
         "Operating System :: Unix",
         "Operating System :: MacOS",
     ],
     python_requires=">=3.9",
-    install_requires=[
-        # ADD ME
-    ],
+    install_requires=[],
     entry_points={
         "console_scripts": [
-            "ssl-metrics-MODULE-NAME-collect = ssl_metrics_MODULE_NAME.FILENAME:main",
-            "ssl-metrics-MODULE-NAME-graph = ssl_metrics_MODULE_NAME.create_graph:main",
+            "pp-me = pp-me.main:main",
         ]
     },
 )
